841b1fbd94e61d2b7961369e49bc9afc6463cd7b
Matute <matiasnlamperti@gmail.com>
2021-10-31 17:56:30 -0300

    2da_Entrega_PF

---
+++ js/producto.js
@@ -0,0 +1,31 @@
+// Variables
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Gorra "La Quimera"',
+        precio: 1200,
+        imagen: '../image/default.jpg'
+    },
+    {
+        id: 2,
+        nombre: 'Remera "La Quimera"',
+        precio: 2500,
+        imagen: '../image/default.jpg'
+    },
+    {
+        id: 3,
+        nombre: 'El Veneno de tu Soledad - CD',
+        precio: 1400,
+        imagen: '../image/default.jpg'
+    },
+    {
+        id: 4,
+        nombre: 'El Momento Indicado - CD',
+        precio: 1700,
+        imagen: '../image/default.jpg'
+    },
+
+
+
+
+];
